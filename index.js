8cb7aa636b3678c5b0a178132e22f1d676441bfd
Dimitris Tsiktsiris <dtsiktsiris@outlook.com>
2021-03-08 19:33:30 +0200

    chore changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-
+    ws.send(message)
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
