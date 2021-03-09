e35f8eb40bbd8b16ec37e331801587dc1a1e9b7f
Dimitris Tsiktsiris <dtsiktsiris@outlook.com>
2021-03-09 14:09:49 +0200

    chore changes

--- index.js
+++ index.js
@@ -3,23 +3,40 @@ const app = express()
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ server:server });
+const wss = new WebSocket.Server({ server: server });
 
-wss.on('connection', function connection(ws,req) {
+wss.on('connection', function connection(ws, req) {
   console.log('A new client Connected!');
+
   ws.send('Welcome New Client!');
-  console.log(req.url);
+  // console.log(req.url);
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-    ws.send(message)
+
+    ws.choice = message;
+    // console.log(ws.choice);
+    // const parameters = url.parse(req.url, true);
+
+    // ws.uid = wss.getUniqueID();
+    // ws.chatRoom = {uid: parameters.query.myCustomID};
+    let list = [];
+    
+    console.log(list);
+    
     wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+      if (client.choice != null) {
+        list.push(client.choice)
       }
     });
-    
+
+    wss.clients.forEach(function each(client) {
+      // if (client !== ws && client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(list));
+      // }
+    });
+
   });
-  ws.on('close',function closing(code,reason){
+  ws.on('close', function closing(code, reason) {
     console.log("closing");
   })
 });
